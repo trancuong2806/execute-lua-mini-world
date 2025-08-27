de3a0e7ba8ac57b038baff09bee1d27052410e2c
Trần Cường <140365769+trancuong2806@users.noreply.github.com>
2025-08-27 15:57:08 +0700

    Update executor.c

--- executor.c
+++ executor.c
@@ -38,7 +38,7 @@ INT_PTR CALLBACK DlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 	}
     case WM_COMMAND:
         if (LOWORD(wParam) == 1002) { // button
-            char buffer[1024 * 1024];
+            char buffer[1024];
             GetDlgItemTextA(hwndDlg, 1001, buffer, sizeof(buffer));
 
             if (luaL_loadstring(gL, buffer) == 0) {
@@ -118,3 +118,4 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     }
     return TRUE;
 }
+
